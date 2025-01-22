80427c74d407659839c0ba4884f19d470aee70b3
ChidiNweze <57571222+ChidiNweze@users.noreply.github.com>
2025-01-22 15:46:56 +0000

    Deploying to docs from @ ChidiNweze/react-chat@5760ccc2da151dea617f7650b10147de412f4333 🚀

---
+++ assets/Footer.story-CATYlA5Z.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime-DRTy3Uxn.js";import{F as a,C as l}from"./index-B-iCD6hf.js";import"./index-BBkUAzwr.js";import"./index-Bjyw5yqY.js";import"./constants-CnzOgqsw.js";import"./tag-CD_1nE8q.js";import"./animation-LM1V2pjp.js";import"./index-k-mBE7b6.js";import"./variants-BbyesEZ0.js";import"./index-B792wbPy.js";import"./device-WPWeBsw9.js";import"./index-DMkto-UT.js";import"./index-8wHEbfWR.js";import"./top-caret-CFqa3nzJ.js";import"./index-D9IMDxHf.js";import"./functional-ii7S4iDS.js";import"./extends-CF3RwP-h.js";import"./index--I7-kaqz.js";import"./index-CX0VroFg.js";import"./index-BUrDBgG0.js";import"./index-BfNzsk-l.js";import"./index-D21AaJ_Q.js";import"./_createSet-zuc-p8Rl.js";import"./_baseToString-BV3LLLfG.js";import"./is-plain-object-BrWfeXm7.js";import"./index-DL_PF9NM.js";import"./index-BNrZ85bq.js";import"./index-Dn3NLog5.js";import"./index-PqR-_bA4.js";import"./index-BZREYrMm.js";const Q={title:"Components/Chat/Footer",component:a,argTypes:{onStart:{action:"onStart"},onSend:{action:"send"}},args:{hasEnded:!1,withWatermark:!1},render:h=>e.jsx(l.Container,{children:e.jsx(a,{...h})})},r={},t={args:{hasEnded:!0}},o={args:{withWatermark:!0}};var m,s,i;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var p,n,c;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    hasEnded: true
+  }
+}`,...(c=(n=t.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var d,u,g;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    withWatermark: true
+  }
+}`,...(g=(u=o.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};export{t as Ended,r as Running,o as WithWatermark,Q as default};
---
+++ assets/UserResponse.story-BjdQdzRf.js
@@ -0,0 +1,34 @@
+import{j as a}from"./jsx-runtime-DRTy3Uxn.js";import{U as n,C as c}from"./index-B-iCD6hf.js";import"./index-BBkUAzwr.js";import"./index-Bjyw5yqY.js";import"./constants-CnzOgqsw.js";import"./tag-CD_1nE8q.js";import"./animation-LM1V2pjp.js";import"./index-k-mBE7b6.js";import"./variants-BbyesEZ0.js";import"./index-B792wbPy.js";import"./device-WPWeBsw9.js";import"./index-DMkto-UT.js";import"./index-8wHEbfWR.js";import"./top-caret-CFqa3nzJ.js";import"./index-D9IMDxHf.js";import"./functional-ii7S4iDS.js";import"./extends-CF3RwP-h.js";import"./index--I7-kaqz.js";import"./index-CX0VroFg.js";import"./index-BUrDBgG0.js";import"./index-BfNzsk-l.js";import"./index-D21AaJ_Q.js";import"./_createSet-zuc-p8Rl.js";import"./_baseToString-BV3LLLfG.js";import"./is-plain-object-BrWfeXm7.js";import"./index-DL_PF9NM.js";import"./index-BNrZ85bq.js";import"./index-Dn3NLog5.js";import"./index-PqR-_bA4.js";import"./index-BZREYrMm.js";const ee={title:"Components/Chat/UserResponse",component:n,args:{timestamp:Date.now()},render:L=>a.jsx(c.Container,{children:a.jsx(c.Dialog,{css:{padding:"64px 0",display:"flex",flexDirection:"column",justifyContent:"center"},children:a.jsx(n,{...L})})})},r={args:{message:"Lorem ipsum dolor"}},s={args:{message:"consecteturaconse cteturaconsecteturaconsecteturaconsecteturaconsectetura consecteturaconsecteturaconsecteturaconsecteturaconsecteturaconsectetura"}},t={args:{message:"Lorem ipsum dolor",debug:{message:"Intent Name (97%)"}}},e={args:{message:"Lorem ipsum dolor",debug:{message:"Intent Name (97%)",reason:"Voluptatum quae, accusamus excepturi inventore ex quos veritatis eaque ab non?"}}},o={args:{...e.args,debug:{message:"Intent Name (97%)",action:{label:"Add Missing Utterance"}}}};var m,i,u;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    message: 'Lorem ipsum dolor'
+  }
+}`,...(u=(i=r.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};var p,g,d;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    message: 'consecteturaconse cteturaconsecteturaconsecteturaconsecteturaconsectetura consecteturaconsecteturaconsecteturaconsecteturaconsecteturaconsectetura'
+  }
+}`,...(d=(g=s.parameters)==null?void 0:g.docs)==null?void 0:d.source}}};var l,b,x;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    message: 'Lorem ipsum dolor',
+    debug: {
+      message: 'Intent Name (97%)'
+    }
+  }
+}`,...(x=(b=t.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var D,f,j;e.parameters={...e.parameters,docs:{...(D=e.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    message: 'Lorem ipsum dolor',
+    debug: {
+      message: 'Intent Name (97%)',
+      reason: 'Voluptatum quae, accusamus excepturi inventore ex quos veritatis eaque ab non?'
+    }
+  }
+}`,...(j=(f=e.parameters)==null?void 0:f.docs)==null?void 0:j.source}}};var q,C,I;o.parameters={...o.parameters,docs:{...(q=o.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    ...DebugReason.args,
+    debug: {
+      message: 'Intent Name (97%)',
+      action: {
+        label: 'Add Missing Utterance'
+      }
+    }
+  }
+}`,...(I=(C=o.parameters)==null?void 0:C.docs)==null?void 0:I.source}}};export{o as ActionableDebugReason,t as Debug,e as DebugReason,r as PlainText,s as Wrapping,ee as default};
